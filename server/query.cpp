9485a33fb8acb0f4ed26ba80886f304ec4952165
Schronk <damian.sirbu01@e-uvt>
2020-11-20 20:42:13 +0200

    progress on reading the database

--- server/query.cpp
+++ server/query.cpp
@@ -8,6 +8,26 @@ void query::run()
 {
     qDebug() << "Query = " << comm;
     QSqlQuery que = db->exec(QString(comm));
+    int i = 0;
+    QString buffer="{\n";
+    QSqlDriver* driver = database.driver();
+    while(que.next())
+    {
+        i=0;
+
+        while(que.value(i).isValid())
+        {
+            qDebug()<<driver->record("users").fieldName(i);
+            //qDebug()<<"i este: "<<i;
+            if(i)
+            {
+
+            }
+            qDebug()<<que.value(i).toString();
+            i++;
+
+        }
+    }
 
 
 }
